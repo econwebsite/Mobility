d83464c31d7dc23c9bb0aff240263fb4a68e35e7
suresh.manivannan <suresh.manivannan@e-consystems.com>
2024-10-28 16:21:31 +0530

    card changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import NavBar from "./Components/NavBarcomp/NavBar";
-import TotalHomePage from "./Components/HomePagecomp/TotalHomePage";
+import Totalhome from "./Components/Homecomp/Totalhome";
 import Footerpage from "./Components/FooterComp/Footerpage";
-import TotalSurroundview from "./Components/Productcomp/SurroundviewProduct/TotalSurroundview";
-import Totalforwardfacing from "./Components/Productcomp/ForwardfacingProduct/Totalforwardfacing";
-import Totalmonitoring from "./Components/Productcomp/DriverMonitoringProduct/Totalmonitoring";
-import Partner from "./Components/PartnersComp/Partner"
-import HubManufacturing from './Components/HubPageComp/ManufacturingHub/HubManufacturing';
 import ScrollToTop from "./Components/ScrollToTop"
-import Cameramobility from "./Components/HomePagecomp/CameraMobility/Cameramobility"
-import BlogHubpage from './Components/HubPageComp/BlogHubpage/BlogHubpage';
-import Casehubpage from "./Components/HubPageComp/CasestudyHubpages/Casehubpage"
-import ContactUs from './Components/HomePagecomp/Homecontactus/ContactUs';
-import HdrLedBlog from "./Components/FutureBlog/HdrLedBlog"
-import DrivermonBlog from "./Components/FutureBlog/DrivermonBlog"
-import ForwardBlog from "./Components/FutureBlog/ForwardBlog"
-import SurroundBlog from "./Components/FutureBlog/SurroundBlog"
 import NotFoundPage from "./Components/Notfoundpage"
-
+import Totalproducts from './Components/ProductComp/Totalproducts';
+import BlogHub from "./Components/HubPageComp/BlogHubcomp/BlogHub"
 const App = () => {
   return (
 
@@ -30,21 +19,13 @@ const App = () => {
     <div className='fixed-container'>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<TotalHomePage />} />
-        <Route path="/surroundview" element={<TotalSurroundview/>} />
-        <Route path="/forwardfacing" element={<Totalforwardfacing/>} />
-        <Route path="/drivermonitoring" element={<Totalmonitoring/>} />
-        <Route path="/Partner" element={<Partner/>} />
-        <Route path="/Cameramobility" element={<Cameramobility/>} />
-        <Route path="/ContactUs" element={<ContactUs/>} />
-        <Route path="/BlogHubpage" element={<BlogHubpage/>} />
-        <Route path="/CasestudyHubpage" element={<Casehubpage/>} />
-        <Route path="/HubManufacturing" element={<HubManufacturing/>} />
-        <Route path="/HdrLedBlog" element={<HdrLedBlog/>} />
-        <Route path="/DrivermonBlog" element={<DrivermonBlog/>} />
-        <Route path="/ForwardBlog" element={<ForwardBlog/>} />
-        <Route path="/SurroundBlog" element={<SurroundBlog/>} />
+        <Route path="/" element={<Totalhome />} />
+        <Route path="/shelfTrack" element={<Totalproducts />} />
+        <Route path="/BlogHubPage" element={<BlogHub />} />
+
+        
         <Route path="*" element={<NotFoundPage />} />
+        
         </Routes>
         <Footerpage/>
 
--- src/Components/NavBarcomp/NavBar.jsx
+++ src/Components/NavBarcomp/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Dropdown, Menu } from 'antd';
 import { Navbar, Nav, Container, Offcanvas, Form, Button } from 'react-bootstrap';
 import './Navbar.css';
-import dentallogo from "../../assets/homepage/NewDentallogo.svg";
+import shelflogo from "../../assets/homepage/shelfmonitor-logo.svg";
 import AnimationButton from '../ButtonComp/AnimationButton';
 
 const NavBar = () => {
@@ -28,13 +28,13 @@ const NavBar = () => {
   const SolutionMenu = (
     <Menu  style={{width:"300px",border:"1px solid #00aeef"}}>
       <Menu.Item key="1">
-        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="/surroundview" onClick={handleNavLinkClick}>Surround View Cameras</Link>
+        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="" onClick={handleNavLinkClick}>Product 1</Link>
       </Menu.Item>
       <Menu.Item key="2">
-        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="/forwardfacing" onClick={handleNavLinkClick}>Forward Facing Cameras</Link>
+        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="" onClick={handleNavLinkClick}>Product 2</Link>
       </Menu.Item>
       <Menu.Item key="3">
-        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="/drivermonitoring" onClick={handleNavLinkClick}>Driver Monitoring Cameras</Link>
+        <Link style={{ textDecoration: "none", color: "#344ea1", fontSize: "medium" }} to="" onClick={handleNavLinkClick}>Product 3</Link>
       </Menu.Item>
     </Menu>
   );
@@ -74,15 +74,16 @@ const NavBar = () => {
           <div className="navbarpage-header">
             <div className="navbarpage-logoContainer">
               <Link to="/">
-                <img src={dentallogo} alt="Dental Logo" className="navbarpage-logo" onClick={() => navigate('/Aguidtochoose')} style={{ cursor: "pointer" }} />
+                <img src={shelflogo} alt=" Logo" className="navbarpage-logo" onClick={() => navigate('/Aguidtochoose')} style={{ cursor: "pointer" }} />
               </Link>
             </div>
             <div className="navbarpage-navItems">
             <Link to="/" className="navbarpage-navLink">Home</Link>
             <div className="navbarpage-divider"></div>
-              <Dropdown overlay={SolutionMenu} placement="bottomLeft" trigger={['hover']}>
-                <Link className="navbarpage-navLink">Solutions</Link>
-              </Dropdown>
+              {/* <Dropdown overlay={SolutionMenu} placement="bottomLeft" trigger={['hover']}>
+                <Link className="navbarpage-navLink">ShelfTrack</Link>
+              </Dropdown> */}            <Link to="/shelfTrack" className="navbarpage-navLink">ShelfTrack</Link>
+
               <div className="navbarpage-divider"></div>
               {/* <Dropdown overlay={productMenu} placement="bottomLeft" trigger={['hover']}>
                 <Link className="navbarpage-navLink">Products</Link>
@@ -91,7 +92,7 @@ const NavBar = () => {
                 <Link className="navbarpage-navLink">Resources</Link>
               </Dropdown>
               <div className="navbarpage-divider"></div>
-              <Link to="/Partner" className="navbarpage-navLink">Partners</Link>
+              <Link to="/Partner" className="navbarpage-navLink">About Us</Link>
               <div className="navbarpage-divider"></div>
               <Link to="/ContactUs" className="navbarpage-navLink">Contact Us</Link>
             </div>
@@ -106,7 +107,7 @@ const NavBar = () => {
                     <p className="navbarpage-phoneNumber">+1 408 766 7503</p>
                   </a>
                 </div>
-                {/* <AnimationButton text="Get a Quote" backgroundColor="#00aeef" className='Navbargetquote' hoverColor="#00aeef" animationColor="#69ba2f" /> */}
+                <AnimationButton text="Get a Quote" backgroundColor="#ffc107" textColor="#131b2d" hoverBackgroundColor="red" hoverTextColor="#344ea1" className='Navbargetquote' />
               </div>
             </div>
           </div>
@@ -118,7 +119,7 @@ const NavBar = () => {
         <Container fluid>
           <Navbar.Brand href="#">
             <Link to="/">
-              <img src={dentallogo} alt="Dental Logo" className="navbarpage-logo-mobile" />
+              <img src={shelflogo} alt="Logo" className="navbarpage-logo-mobile" />
             </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
