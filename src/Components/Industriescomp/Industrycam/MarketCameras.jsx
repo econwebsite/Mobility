63fc39019029aa80b339ea8ccbd74289efc36c01
suresh.manivannan <suresh.manivannan@e-consystems.com>
2025-04-06 20:22:11 +0530

    css path

--- src/Components/Industriescomp/Industrycam/MarketCameras.jsx
+++ src/Components/Industriescomp/Industrycam/MarketCameras.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Marketcameras.css';
+import './marketcameras.css';
 
 const Marketcameras = ({ cameras }) => {
   const navigate = useNavigate();
