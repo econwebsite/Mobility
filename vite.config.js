403e553777c7c9967c0e195a4046c7120c573ac2
suresh.manivannan <suresh.manivannan@e-consystems.com>
2025-04-06 17:55:34 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/slick-carousel/slick/fonts/*',
-          dest: 'fonts'
-        }
-      ]
-    })
-  ]
-});
+  plugins: [react()],
+})
